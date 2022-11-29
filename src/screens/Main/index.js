9ce8b8b0c514c89c97cb12cbd34575e1a1e129e1
fadhildev <safadhildev@gmail.com>
2022-11-30 04:29:10 +0800

    update main screen ui

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,16 +1,7 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, StatusBar } from 'react-native';
 import { Button, Container } from '../../components';
+import colors from '../../components/colors';
 import constants from '../../components/constants';
-
-const styles = StyleSheet.create({
-  buttonWrapper: {
-    padding: 20,
-  },
-  wrapper: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
 
 const Main = ({ navigation }) => {
   const onNavigate = (routeName) => {
@@ -18,27 +9,68 @@ const Main = ({ navigation }) => {
   };
 
   return (
-    <Container>
-      <View style={styles.wrapper}>
-        <View style={styles.buttonWrapper}>
-          <Button
-            text="Contact Us"
-            onPress={() => {
-              onNavigate(constants.routes.ContactUs);
-            }}
-          />
+    <>
+      <View style={styles.container}>
+        <StatusBar translucent barStyle="light-content" backgroundColor="transparent" />
+        <View style={styles.topContainer}></View>
+        <View style={styles.border} />
+        <View style={styles.middleContainer}>
+          <View style={styles.outerRing}>
+            <View style={styles.innerRing} />
+          </View>
         </View>
-        <View style={styles.buttonWrapper}>
-          <Button
-            text="View Catalog"
-            onPress={() => {
-              onNavigate(constants.routes.Catalog);
-            }}
-          />
-        </View>
+        <View style={styles.bottomContainer}></View>
       </View>
-    </Container>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  border: {
+    backgroundColor: colors.black,
+    height: 10,
+  },
+  middleContainer: {
+    zIndex: 1,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topContainer: { flex: 1, backgroundColor: colors.red },
+  bottomContainer: { flex: 1, backgroundColor: colors.white },
+  buttonWrapper: {
+    padding: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  outerRing: {
+    position: 'relative',
+    borderColor: '#222225',
+    borderRadius: 100,
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    backgroundColor: '#F0F0F0',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerRing: {
+    position: 'absolute',
+    borderColor: '#222225',
+    borderRadius: 100,
+    width: 50,
+    height: 50,
+    borderWidth: 3,
+    backgroundColor: '#F0F0F0',
+  },
+});
 
 export default Main;
